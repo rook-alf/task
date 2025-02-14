a16c03089269e790d953874eb4ceaf1545cf1794
My name <myEmail@example.com>
2025-02-14 17:30:17 +0400

    change task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,83 +1,24 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Net.Sockets;
+﻿
+
 using System.Text;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using static System.Net.Mime.MediaTypeNames;
 
 internal class Program
 {
-     public static void Main(string[] args)
-     {
-        string filePathValue = args[0];
-        string filePathTests = args[1];
-        string filePathReport = args[2];
-
-        ResultValues? values = JsonSerializer.Deserialize<ResultValues>(File.ReadAllText(filePathValue));
-        ResultTests? tests = JsonSerializer.Deserialize<ResultTests>(File.ReadAllText(filePathTests));       
-        
-        Dictionary<int, ItemT> map = new Dictionary<int, ItemT>();
-        FillMap(tests.Tests, map);
-
-        
-        foreach (var value in values.Values)
-        {
-            if (map.ContainsKey(value.Id))
-            {
-                map[value.Id].Value = value.Value;
-            }
-        }
-        string json = JsonSerializer.Serialize(tests);
-        File.WriteAllText(filePathReport, json);
-     }
-
-    public static void FillMap(ItemT[] items, Dictionary<int, ItemT> map) 
+    public static void Main(string[] args)
     {
-        if (items == null) return;
-        foreach (var item in items)
+        string[] file = File.ReadAllLines(args[0], Encoding.Default);
+        int[] mas = file.Select(int.Parse).ToArray();     
+        Array.Sort(mas);
+        int avgIndex = (mas.Length/2);
+        int mediana = mas[avgIndex];
+        int sum = 0;
+        for (int i = 0; i < mas.Length; i++) 
         {
-            map.Add(item.Id, item);
-            FillMap(item.Values, map);
+            sum = Math.Abs(mas[i] - mediana) + sum;
         }
-    }
-}
-
- 
-public class ResultValues
-{
-    [JsonPropertyName("values")]
-    public ItemV[] Values { get; set; }
-}
-
-public class ItemV
-{
-    [JsonPropertyName("id")]
-    public int Id { get; set; }
-
-    [JsonPropertyName("value")]
-    public string Value { get; set; }
-}
-
-public class ResultTests
-{
-    [JsonPropertyName("tests")]
-    public ItemT[] Tests { get; set; }
+        Console.WriteLine(sum);
+    } 
 }
 
 
-public class ItemT
-{
-    [JsonPropertyName("id")]
-    public int Id { get; set; }
-   
-    [JsonPropertyName("title")]
-    public string Title { get; set; }
-    
-    [JsonPropertyName("value")]
-    public string Value { get; set; }
-    
-    [JsonPropertyName("values")]
-    public ItemT[] Values { get; set; }
-}
+
